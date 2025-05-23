6a10e61696a64675338f1305c262b268d8d68993
KaykyRocha016 <kaykyrocha97@gmail.com>
2025-05-23 01:14:04 -0300

    Refactor: mudança do tipo de Id da Task de Int para UUID

--- app/src/main/java/com/example/personaltasks/model/Task.kt
+++ app/src/main/java/com/example/personaltasks/model/Task.kt
@@ -2,9 +2,10 @@ package com.example.personaltasks.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.util.UUID
 
 @Parcelize
-data class Task (val id: Int?, val title: String, val description: String, val deadline : String) :
+data class Task (val id: UUID?, val title: String, val description: String, val deadline : String) :
     Parcelable {
 
 
