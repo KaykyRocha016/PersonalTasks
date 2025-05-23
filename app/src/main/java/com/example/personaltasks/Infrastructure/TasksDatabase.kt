f3a74a71909ad2bc55798d87c279df2f39b9882a
KaykyRocha016 <kaykyrocha97@gmail.com>
2025-05-23 03:34:04 -0300

    corrigindo anotações da classe, @TypeConverters(TypeConverters::class)->@TypeConverters(UUIDConverter::class)

--- app/src/main/java/com/example/personaltasks/Infrastructure/TasksDatabase.kt
+++ app/src/main/java/com/example/personaltasks/Infrastructure/TasksDatabase.kt
@@ -3,11 +3,12 @@ package com.example.personaltasks.Infrastructure
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.personaltasks.Utils.UUIDConverter
 import com.example.personaltasks.model.Task
 import com.example.personaltasks.model.TaskDao
 
 @Database(entities = [Task::class], version = 1)
-@TypeConverters(TypeConverters::class)
+@TypeConverters(UUIDConverter::class)
 abstract class TasksDatabase:RoomDatabase() {
     abstract fun taskDao(): TaskDao
 }
