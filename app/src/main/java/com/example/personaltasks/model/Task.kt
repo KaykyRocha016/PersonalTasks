63ba6346cfa1e78ccf9acf4bc3c93fb60d4ca0c5
KaykyRocha016 <kaykyrocha97@gmail.com>
2025-06-27 00:19:35 -0300

    Refactor: deletedAt and deleted field added to TaskModel

--- app/src/main/java/com/example/personaltasks/model/Task.kt
+++ app/src/main/java/com/example/personaltasks/model/Task.kt
@@ -2,13 +2,14 @@ package com.example.personaltasks.model
 
 import android.os.Parcelable
 import androidx.room.Entity
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
 import kotlinx.parcelize.Parcelize
 
 //modelo de Task
 @Parcelize
 @Entity(tableName = "task")
-data class Task (@DocumentId val id: String, val title: String, val description: String, val deadline : String, var completed: Boolean) :
+data class Task (@DocumentId val id: String, val title: String, val description: String, val deadline : String, var completed: Boolean , var deletedAt : Timestamp? =null,var deleted: Boolean = false ) :
     Parcelable {
 
 
